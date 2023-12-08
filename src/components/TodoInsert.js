a84c2cae7994461b30272478df058b9ea96f8b3d
121Mbp <nino09@naver.com>
2023-12-08 14:30:29 +0900

    url

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -65,7 +65,7 @@ const TodoInsert = ({ onInsertTodo, source }) => {
             </div>
             <button onClick={ handleSubmit }>Add Todo</button>
             <p>
-                <a href='https://react.dev/'>Based on React</a>
+                <a href='https://react.dev/' target='_blank'>Based on React</a>
                 <a href='https://github.com/121Mbp' target='_blank'>Github</a>
             </p>
         </div>
